5a568f6e4a184e8a98b0708e89ab0bcf37ec30ea
Carlolis <cilieff@gmail.com>
2025-03-05 15:57:04 +0100

    update react router

--- app/root.tsx
+++ app/root.tsx
@@ -65,7 +65,7 @@ const Navigation = ({ isAuthenticated }: NavigationPros) => (
         <div className="flex items-center">
           {isAuthenticated ?
             (
-              <NavLink to={href('/logout')}>
+              <NavLink to={href('/')}>
                 <button
                   type="submit"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
--- app/routes.ts
+++ app/routes.ts
@@ -3,7 +3,7 @@ import { route, type RouteConfig } from "@react-router/dev/routes";
 
 // export default flatRoutes() satisfies RouteConfig;
 export default [
-  route("/", "./routes/index.tsx"),
+  route("", "./routes/index.tsx"),
   route("/dashboard", "./routes/dashboard.tsx"),
   route("/login", "./routes/login.tsx"),
   route("/trip/new","./routes/trip.new.tsx"),
